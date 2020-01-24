f84703a6e0f5e7444a59b6036b2d42158dafa637
DThalia <daniithaliarp@gmail.com>
2020-01-24 12:39:28 -0500

    Se COMPLETO LOS ARCHIVOS PARA OFICINAS

--- moduloA/controllers/oficinas.controller.php
+++ moduloA/controllers/oficinas.controller.php
@@ -1,11 +1,11 @@
 <?php 
 require("../models/oficinas.model.php");
 
-$nombre_of = $_POST['nom_oficina'];
+$nombre_of = trim(strtoupper($_POST['nombre_of']));
 $id_metas = $_POST['id_metas'];
 $id_institucion = $_POST['id_institucion'];
 
-$oficina = new Oficina();
+$oficina = new Oficinas();
 $oficina->Guardar($nombre_of,$id_metas,$id_institucion);
 
 header("Location: ../index.php");
--- moduloA/models/oficinas.model.php
+++ moduloA/models/oficinas.model.php
@@ -15,7 +15,7 @@ class Oficinas
 	public function Guardar($nombre_of,$id_metas,$id_institucion)
 	{
 		$fechaActual = date('Y-m-d H:i:s');
-		$sql = "INSERT INTO oficinas VALUES (null,'$nombre_of','$fechaActual','$fechaActual','$id_metas',id_institucion')";
+		$sql = "INSERT INTO oficinas VALUES (null,'$nombre_of','$fechaActual','$fechaActual','$id_metas','$id_institucion')";
 
 		if(!$this->conn->query($sql)){
 			echo "Error: " . mysqli_error();
@@ -31,7 +31,11 @@ class Oficinas
 
 	public function Consultar()
 	{
+		$sql = "SELECT id, nombre_of, f_creacion,f_update,id_metas,id_institucion FROM oficinas;";
 		
+		$response = $this->conn->query($sql);
+
+		return $response;
 	}
 
 	public function CrearOficinas()
