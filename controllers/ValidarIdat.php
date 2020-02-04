f715c425faf5a2eaee22c1c5bb2dba41b35a4f78
DThalia <daniithaliarp@gmial.com>
2020-02-04 13:05:52 -0500

    Agregando validacion

--- controllers/ValidarIdat.php
+++ controllers/ValidarIdat.php
@@ -1,10 +1,37 @@
 <?php 
+session_start();
+require "../Core/Conexion.php";
+$conn=new Conexion();
+$conexion=$conn->Conectar();
 
 $username = $_POST['username'];
 $passwd = $_POST['clave'];
 
 printf("Usuario: %s con Contraseña: %s", $username, $passwd);
 
-header("Location: ../moduloA/index.php");
+$sql = "SELECT niv_usu, id_personal FROM login WHERE nom_usu='$username' and psw_usu='$passwd' and chk_usu=1;";
 
-?>
+$response=$conexion->query($sql);
+$data=$response->fetch_array(MYSQLI_ASSOC);
+
+if ($data['niv_usu'] != null) {
+	
+ echo $data['niv_usu'];
+}else{
+	echo "error";
+}
+
+switch ($data['niv_usu']) {
+	case '1':
+		$_SESSION["admin"]=$data["id_personal"];
+		header("location: ../moduloA/index.php");
+		break;
+	case '2':
+	    $_SESSION["id_personal"]=$data['id_personal'];
+        header("location: ../moduloB/index.php");
+	    break;
+	
+	default:
+		# code...
+		break;
+}
