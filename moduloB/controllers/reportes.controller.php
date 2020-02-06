eaf728f3ecec638b70e012bb19315a260d5f6608
DThalia <daniithaliarp@gmial.com>
2020-02-06 13:03:25 -0500

    modificando controller/reportes.controller

--- moduloB/controllers/reportes.controller.php
+++ moduloB/controllers/reportes.controller.php
@@ -3,12 +3,12 @@ require("../models/reportes.model.php");
 
 
 $id_personal = $_POST['id_personal'];
-$id_responsabilidades = $_POST['id_responsabilidades'];
+$id_acciones = $_POST['id_acciones'];
 $f_reportes = $_POST['f_reportes'];
 $cantidad_avance = $_POST['cantidad_avance'];
 
 $reportes = new Reportes();
-$reportes->Guardar($id_personal,$id_responsabilidades,$f_reportes,$cantidad_avance);
+$reportes->Guardar($id_personal,$id_acciones,$f_reportes,$cantidad_avance);
 
 header("Location: ../index.php");
 
