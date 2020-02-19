83edae93ae2b722f6c71de7fd96419ce13f520ea
corayma nuñez quispe <coraymanq@gmail.com>
2020-02-19 12:43:08 -0500

    cambio de nombre a bd limpia en las conexiones

--- moduloB/models/Conexion.php
+++ moduloB/models/Conexion.php
@@ -5,7 +5,7 @@ class Conexionn {
 	public function Conectar() {
 		#require_once ("config.php");
 
-		$mysqli = new mysqli("localhost","coraima", "A@dmin0215.,$", "bd_institucion");
+		$mysqli = new mysqli("192.168.0.73","usuario", "archivo123$", "avancemetas");
 		$mysqli->set_charset("utf8");
 		if ($mysqli->connect_errno) {
 			echo "Error al contenctar a MySQL: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
