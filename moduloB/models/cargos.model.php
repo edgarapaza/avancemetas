0a6e654bb273f80e66710249f4cf25a88c5a9be0
Edgar Apaza <edgarapazac@gmail.com>
2021-06-25 13:19:35 -0500

    Cambios en el Modulo B - Plantilla

---
+++ moduloB/models/cargos.model.php
@@ -0,0 +1,67 @@
+<?php
+require_once "Conexion.php";
+
+class Cargo
+{
+	private $conn;
+
+	function __construct()
+	{
+		$this->conn = new Conexion();
+		return $this->conn;
+	}
+
+	public function Guardar($nombre,$id_oficina,$id_areas,$id_personal)
+	{
+		$fechaActual = date('Y-m-d H:i:s');
+		
+		$sql = "INSERT INTO cargos (id_cargos,nombre,id_oficina,id_areas,id_personal,fec_creacion) VALUES (null,'$nombre','$id_oficina','$id_areas','$id_personal','$fechaActual');";
+
+		$res = $this->conn->ConsultaSin($sql);
+        return $res;
+
+	}
+
+	public function Modificar($idcargos,$nombre)
+	{
+		$sql = "UPDATE cargos SET nombre = '$nombre' WHERE id_cargos = '$idcargos';";
+
+		$res = $this->conn->ConsultaCon($sql);
+        return $res;
+	}
+
+	public function Consultar()
+	{
+		$sql = "SELECT id_cargos,nombre,id_oficina,id_areas,id_personal,fec_creacion FROM cargos;";
+		
+		$res = $this->conn->ConsultaCon($sql);
+        return $res;
+	}
+
+	public function MostrarCargos($idcargos)
+	{
+		$sql = "SELECT id_cargos,nombre,id_oficina,id_areas,id_personal FROM cargos WHERE id_cargos = $idcargos;";
+		
+		$res = $this->conn->ConsultaArray($sql);
+        return $res;
+	}
+
+	public function NombrePersonal($idpersonal)
+    {
+        $fechaActual = date('Y-m-d H:i:s');
+
+        $sql = "SELECT CONCAT(nombre,' ',apellidos) AS personal, foto FROM personal WHERE id_personal = $idpersonal;";
+
+        $res = $this->conn->ConsultaArray($sql);
+        return $res;
+    }
+
+    	public function MostrarCargosAccion($idcargos)
+	{
+
+		$sql = "SELECT nombre FROM cargos WHERE id_cargos = $idcargos;";
+		
+		$res = $this->conn->ConsultaArray($sql);
+        return $res;
+	}
+}
